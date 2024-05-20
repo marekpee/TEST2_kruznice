92790fc70981ac30a46fa13787da45919fbdd69a
marek <marek@PETR>
2024-05-20 07:06:17 +0200

    Přidejte soubory projektu.

---
+++ TEST2_kruznice/Models/Kruhy.cs
@@ -0,0 +1,25 @@
+﻿namespace TEST2_kruznice.Models
+{
+    public class Kruhy
+    {
+        public List<PosunStredy> KruzniceList {  get; set; } = new List<PosunStredy>();
+        public PosunStredy PosunStredy { get; set; } = new PosunStredy();
+        public void PridejKruznice()
+        {
+            for (int i = 0; i < PosunStredy.Pocet; i++)
+            {
+                var pomX = PosunStredy.PozX;
+                var pomY = PosunStredy.PozY + i * PosunStredy.Vzdalenost;
+                KruzniceList.Add(new PosunStredy(PosunStredy.Polomer,pomX,pomY));
+            }
+        }
+        public void SmazatVse()
+        {
+            KruzniceList.Clear();
+        }
+        public void SmazatIndex()
+        {
+            KruzniceList.RemoveAt(PosunStredy.IndexMazani-1);
+        }
+    }
+}
---
+++ TEST2_kruznice/Models/PosunStredy.cs
@@ -0,0 +1,23 @@
+﻿namespace TEST2_kruznice.Models
+{
+    public class PosunStredy
+    {
+        public PosunStredy()
+        {
+        }
+
+        public PosunStredy(int polomer, double pozX, double pozY)
+        {
+            Polomer = polomer;
+            PozX = pozX;
+            PozY = pozY;
+        }
+
+        public int Polomer { get; set; }
+        public double PozX { get; set; } = 250;
+        public double PozY { get; set; } = 150;
+        public int Pocet { get; set; }
+        public int Vzdalenost { get; set; }
+        public int IndexMazani { get; set; }
+    }
+}
